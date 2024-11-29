8c7320dbf04a1aed1cecad163ab694c89ec321fa
ValeriaGiannelli <vg.giannellivaleria@gmail.com>
2024-11-29 14:59:02 +0100

    feat: validate the token

--- oauth/google-oauth.php
+++ oauth/google-oauth.php
@@ -27,7 +27,29 @@ if (isset($_GET['code']) && !empty($_GET['code'])) {
     $response = curl_exec($ch);
     curl_close($ch);
     $response = json_decode($response, true);
-    // Code goes here...
+    
+    // Make sure access token is valid
+    if (isset($response['access_token']) && !empty($response['access_token'])) {
+        // Execute cURL request to retrieve the user info associated with the Google account
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.googleapis.com/oauth2/' . $google_oauth_version . '/userinfo');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $response['access_token']]);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        $profile = json_decode($response, true);
+        // Make sure the profile data exists
+        if (isset($profile['email'])) {
+            $google_name_parts = [];
+            $google_name_parts[] = isset($profile['given_name']) ? preg_replace('/[^a-zA-Z0-9]/s', '', $profile['given_name']) : '';
+            $google_name_parts[] = isset($profile['family_name']) ? preg_replace('/[^a-zA-Z0-9]/s', '', $profile['family_name']) : '';
+            // Code goes here...
+        } else {
+            exit('Could not retrieve profile information! Please try again later!');
+        }
+    } else {
+        exit('Invalid access token! Please try again later!');
+    }
 } else {
     // Define params and redirect to Google Authentication page
     $params = [
