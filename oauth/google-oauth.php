a7ae39e12c814f7e9ada3d8d43616fb9d8f4c527
ValeriaGiannelli <vg.giannellivaleria@gmail.com>
2024-11-29 14:57:29 +0100

    feat: add session and variables

---
+++ oauth/google-oauth.php
@@ -0,0 +1,14 @@
+<?php
+
+// faccio partire la sessione
+session_start();
+
+// aggiorno le variabili
+$google_oauth_client_id = "YOUR_CLIENT_ID"; 
+$google_oauth_client_secret = 'YOUR_CLIENT_SECRET';
+$google_oauth_redirect_uri = 'http://localhost/login/oauth/google-oauth.php';
+$google_oauth_version = 'v3';
+
+
+
+?>
